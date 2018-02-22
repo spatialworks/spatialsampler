5821ba08be3dd3c977b2f396301c60e62a53baae
ernestguevarra <ernestgmd@gmail.com>
2018-02-22 05:31:11 +0000

    create package scripts

---
+++ R/data.R
@@ -0,0 +1,99 @@
+################################################################################
+#
+#' gedaref_villages
+#'
+#' A dataset of village names of Gedaref State, Sudan with data on coordinate locations
+#'
+#' @format A data frame with 5 columns and 625 rows:
+#' \describe{
+#' \item{\code{id}}{Unique identifier}
+#' \item{\code{x}}{Longitude (in decimal format)}
+#' \item{\code{y}}{Latitude (in decimal format)}
+#' \item{\code{village}}{Village name}
+#' \item{\code{locality}}{Locality name}
+#' }
+#' @source Sudan Bureau of Statistics and UNICEF Sudan
+#'
+#
+################################################################################
+"gedaref_villages"
+
+
+################################################################################
+#
+#' sennar_villages
+#'
+#' A dataset of village names of Sennar State, Sudan with data on coordinate locations
+#'
+#' @format A data frame with 5 columns and 741 rows:
+#' \describe{
+#' \item{\code{id}}{Unique identifier}
+#' \item{\code{x}}{Longitude (in decimal format)}
+#' \item{\code{y}}{Latitude (in decimal format)}
+#' \item{\code{village}}{Village name}
+#' \item{\code{locality}}{Locality name}
+#' }
+#' @source Sudan Bureau of Statistics and UNICEF Sudan
+#'
+#
+################################################################################
+"sennar_villages"
+
+################################################################################
+#
+#' sennar_villages
+#'
+#' A dataset of village names of Sennar State, Sudan with data on coordinate locations
+#'
+#' @format A data frame with 5 columns and 741 rows:
+#' \describe{
+#' \item{\code{id}}{Unique identifier}
+#' \item{\code{x}}{Longitude (in decimal format)}
+#' \item{\code{y}}{Latitude (in decimal format)}
+#' \item{\code{village}}{Village name}
+#' \item{\code{locality}}{Locality name}
+#' }
+#' @source Sudan Bureau of Statistics and UNICEF Sudan
+#'
+#
+################################################################################
+"sennar_villages"
+
+################################################################################
+#
+#' sudan01
+#'
+#' A map dataset of class SpatialPolygonsDataFrame of level 1 administrative units (states) of Sudan
+#'
+#' @format A SpatialPolygonsDataFrame with 5 features with 4 fields:
+#' \describe{
+#' \item{\code{OBJECTID_1}}{Unique identifier}
+#' \item{\code{STATE}}{State name}
+#' \item{\code{Source}}{Source of data}
+#' \item{\code{Region}}{Region name}
+#' }
+#' @source Sudan Bureau of Statistics and UNICEF Sudan
+#'
+#
+################################################################################
+"sudan01"
+
+################################################################################
+#
+#' sudan02
+#'
+#' A map dataset of class SpatialPolygonsDataFrame of level 2 administrative units (localities) of Sudan
+#'
+#' @format A SpatialPolygonsDataFrame with 5 features and 5 rows:
+#' \describe{
+#' \item{\code{Locality}}{Locality name}
+#' \item{\code{State}}{State name}
+#' \item{\code{Source}}{Source of data}
+#' \item{\code{Status}}{Status of verification}
+#' \item{\code{season2013}}{Season 2013}
+#' }
+#' @source Sudan Bureau of Statistics and UNICEF Sudan
+#'
+#
+################################################################################
+"sudan02"
---
+++ R/s3m.R
@@ -0,0 +1,17 @@
+################################################################################
+#
+#' Simple Spatial Sampling Method (S3M)
+#'
+#' The Simple Spatial Survey Method (S3M) was developed from the Centric
+#' Systematic Area Sampling (CSAS) approach used in biodiversity species
+#' counting and adapted for use in health and nutrition surveys by Brixton Health
+#' and Valid International. S3M is used in surveys designed to be spatially
+#' representative i.e., the sample distributed evenly across the survey area,
+#' using a spatial sample design that selected communities located closest to
+#' the centroids of a hexagonal grid laid over the survey area. The resulting
+#' sample is a triangular irregular network.
+#'
+#' @docType package
+#' @name s3m
+#
+################################################################################
