a70de980f0cb976e7274b4649d007196faa79b64
ernestguevarra <ernestgmd@gmail.com>
2018-02-24 02:36:34 +0000

    update get_nearest_point()

--- R/nearest_point.R
+++ R/nearest_point.R
@@ -6,23 +6,19 @@
 #'
 #' @param input A matrix or data frame of input sampling locations to which nearest
 #'     village locations are to be matched. Data frame should contain at least
-#'     information on longitude and latitude coordinates. Currently, the default
-#'     expected \code{input} is that of an object of class \code{SpatialPoints}
-#'     produced by \code{grid_hexagon()} with longitude and latitude data named
-#'     \code{x} and \code{y} respectively
+#'     information on longitude and latitude coordinates.
+#' @param x1 A character value specifying the variable name in \code{input}
+#'     holding the longitude information of the sampling locations
+#' @param y1 A character value specifying the variable name in \code{input}
+#'     holding the latitude information of the village/community locations
 #' @param query A data frame of village/community locations with at least
 #'     information on longitude and latitude coordinates from which to query for
-#'     nearest point. This argument is optional. If provided, must specify \code{x}
-#'     and \code{y} parameters to identify columns in \code{query} for longitude
-#'     and latitude information
-#' @param x A numeric vector providing longitude of village/community locations
-#'     (default). If \code{query} is specified, \code{x} must be a character value
-#'     specifying the variable name in \code{query} holding the longitude
-#'     information of the village/community locations
-#' @param y A numeric vector providing latitude of village/community locations
-#'     (default). If \code{data} is specified, \code{y} must be a character value
-#'     specifying the variable name in \code{data} holding the latitude
-#'     information of the village/community locations
+#'     nearest point.
+#' @param x2 A character value specifying the variable name in \code{query}
+#'     holding the longitude information of the village/community locations
+#' @param y2 A character value specifying the variable name in \code{query}
+#'     holding the latitude information of the village/community locations
+#' @param n Number of nearest village/community locations to select
 #'
 #' @return A data frame of selected nearest sampling village/community locations
 #'
@@ -37,65 +33,48 @@
 ################################################################################
 
 get_nearest_point <- function(input,
-                              x, y,
-                              query) {
+                              x1, y1,
+                              query,
+                              x2, y2,
+                              n = 1) {
   #
   # Create concatenating object
   #
   near.point <- NULL
   #
-  # Check if data is NULL
+  # Check that x1, y1, x2, y2 are character
   #
-  if(is.null(input)) {
+  if(x1 != "character" | y1 != "character" | x2 != "character" | y2 != "character") {
+    stop("x1 and/or y1 and/or x2 and/or y2 is/are not character. Try again")
+    }
+  #
+  # Cycle through rows of input
+  #
+  for(i in 1:nrow(input)) {
     #
-    # Check that x and y are numeric
+    # Create concatenating vector for distances
     #
-    if(class(x) != "numeric" | class(y) != "numeric") {
-      stop("x and/or y is not numeric. If input is not provided, x and y should be numeric. Try again")
+    near.point1 <- NULL
+    #
+    # Cycle through rows of query
+    #
+    for(j in 1:nrow(query)) {
+      #
+      # Get distance between current sampling point and current village
+      #
+      dist <- tail(Imap::gdist(x1[i], y1[i], x2[j], y2[j], units = "km"), n)
+      #
+      #
+      #
+      near.point1 <- c(near.point1, dist)
     }
     #
-    # Cycle through rows of data
+    # Find the village nearest to the sampling point
     #
-    for(i in 1:length(x)) {
-
-      near.point1 <- Imap::gdist(input$x[i], input$y[i], x, y,
-                                 units = "km")
-
-      near.point2 <-  c(y$id[which(near.point1 == min(near.point1))],
-                        y$x[which(near.point1 == min(near.point1))],
-                        y$y[which(near.point1 == min(near.point1))],
-                        y$village[which(near.point1 == min(near.point1))],
-                        y$locality[which(near.point1 == min(near.point1))],
-                        min(near.point1))
-
-      near.point <- rbind(near.point, near.point2)
-    }
+    near.point2 <- query[which(near.point1 == min(near.point1)), ]
+    #
+    # Concatenate villages
+    #
+    near.point <- data.frame(rbind(near.point, near.point2))
   }
-  #
-  #
-  #
-  for(i in 1:length(data)) {
-
-    near.point1 <- Imap::gdist(data$x[i], x$y[i], y[,2], y[,3],
-                               units = "km")
-
-    near.point2 <-  c(y$id[which(near.point1 == min(near.point1))],
-                      y$x[which(near.point1 == min(near.point1))],
-                      y$y[which(near.point1 == min(near.point1))],
-                      y$village[which(near.point1 == min(near.point1))],
-                      y$locality[which(near.point1 == min(near.point1))],
-                      min(near.point1))
-
-    near.point <- rbind(near.point, near.point2)
-  }
-  near.point <- as.data.frame(near.point)
-  names(near.point) <- c("id", "x", "y", "village", "locality", "d")
-  rownames(near.point) <- NULL
-  near.point[,1] <- as.numeric(near.point[,1])
-  near.point[,2] <- as.numeric(near.point[,2])
-  near.point[,3] <- as.numeric(near.point[,3])
-
-  # Remove duplicates
-  near.point <- near.point[!duplicated(near.point[,-6]),]
-  return(near.point)
 }
