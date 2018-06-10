5be4f78e46fd37f6dcf847e1f2ba5a2756ee3990
Ernest Guevarra <ernest@validmeasures.org>
2018-06-10 11:49:04 +0100

    create calculate_area.R

---
+++ R/calculate_area.R
@@ -0,0 +1,35 @@
+################################################################################
+#
+#' calculate_area
+#'
+#' Function to calculate the area size of the resulting hexagon in a hexagonal
+#' grid and the area size of the resulting triangle in a triangular grid for a
+#' specified value of \code{d} (in kilometres) used in the simple spatial
+#' sampling method (S3M).
+#'
+#' @param d A numeric value for length (in kilometres) of the maximum distance
+#'     of a village/community from a sampling point.
+#' @param digits A numeric value specifying the number of digits to round off
+#'     the resulting area size. Default is 1.
+#'
+#' @return A data.frame of 1 row and 2 columns. First column is for the area
+#'     (in square kilometres) for a triangle. Second column is for the area (in
+#'     square kilometres) for a hexagon.
+#'
+#' @author Ernest Guevarra based on equations by Mark Myatt
+#'
+#' @examples
+#' # Calculate the area of the triangle and hexagon for a d of 10 kilometres
+#' calculate_area(d = 10, digits = 2)
+#'
+#' @export
+#'
+#
+################################################################################
+
+calculate_area <- function(d, digits = 1) {
+  tri.area <- round(tan(30 * (pi / 180)) * (9 / 4) * d ^ 2, digits = digits)
+  hex.area <- round(((3 * sqrt(3)) / 2) * d ^ 2, digits = digits)
+  area <- data.frame("tri" = tri.area, "hex" = hex.area)
+  return(area)
+}
