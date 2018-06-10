9afb90fa10f5369f2de24e4792fcc9a87982b15c
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2018-06-10 14:55:37 +0100

    update calculate_area.R

--- R/calculate_area.R
+++ R/calculate_area.R
@@ -10,7 +10,7 @@
 #' @param d A numeric value for length (in kilometres) of the maximum distance
 #'     of a village/community from a sampling point.
 #' @param digits A numeric value specifying the number of digits to round off
-#'     the resulting area size. Default is 1.
+#'     the resulting area size. Default is NULL
 #'
 #' @return A data.frame of 1 row and 2 columns. First column is for the area
 #'     (in square kilometres) for a triangle. Second column is for the area (in
@@ -27,9 +27,27 @@
 #
 ################################################################################
 
-calculate_area <- function(d, digits = 1) {
-  tri.area <- round(tan(30 * (pi / 180)) * (9 / 4) * d ^ 2, digits = digits)
-  hex.area <- round(((3 * sqrt(3)) / 2) * d ^ 2, digits = digits)
+calculate_area <- function(d, digits = NULL) {
+  #
+  # Calculate triangular area
+  #
+  tri.area <- tan(30 * (pi / 180)) * (9 / 4) * d ^ 2
+  #
+  # Calculate hexagonal area
+  #
+  hex.area <- ((3 * sqrt(3)) / 2) * d ^ 2
+  #
+  # Concatenate calculations
+  #
   area <- data.frame("tri" = tri.area, "hex" = hex.area)
+  #
+  # Round results if !is.null(digits)
+  #
+  if(!is.null(digits)) {
+    round(area, digits = digits)
+  }
+  #
+  # Return output
+  #
   return(area)
 }
