89bac88fb11e972a13af432dca52096c6b5ade88
Ernest Guevarra <ernest@validmeasures.org>
2018-06-17 07:23:41 +0100

    add create_buffer()

---
+++ R/create_buffer.R
@@ -0,0 +1,53 @@
+################################################################################
+#
+#' create_buffer
+#'
+#' Function to create a buffer around a SpatialPolygons or SpatialPolygonsDataFrame
+#' object. This is a wrapper for \code{gBuffer()} function from package \code{rgeos}
+#' adding transformation of planar coordinates to appropriate projected coordinates
+#' to enable creation of buffer.
+#'
+#' @param x A class SpatialPolygons or SpatialPolygonsDataFrame object defining
+#'     the area to be buffered
+#' @param buffer A numeric value for distance (in kilometres) to expand the
+#'     borders of the given spatial object \code{x}. Negative values allowed.
+#' @param country Name of country where sampling area is located. This is used
+#'     to determine the appropriate UTM projection to transform \code{x}
+#'
+#' @return A SpatialPolygons with the same CRS as \code{x}
+#'
+#' @examples
+#' #
+#'
+#' @export
+#'
+#
+################################################################################
+
+create_buffer <- function(x, buffer, country) {
+  #
+  #
+  #
+  x.utm <- sp::spTransform(x = x,
+                           CRSobj = sp::CRS(as.character(map_projections$proj[map_projections$country == country])))
+  #
+  # Add buffer
+  #
+  x.buffer <- rgeos::gBuffer(x.utm, width = buffer * 1000)
+  #
+  #
+  #
+  x.output <- spTransform(x = x.buffer,
+                          CRSobj = sp::CRS(sp::proj4string(x)))
+  #
+  #
+  #
+  return(x.output)
+}
+
+
+
+
+
+
+
