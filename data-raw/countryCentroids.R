88426633b9025a7cb2f4383f1749e2960f015a36
Ernest Guevarra <ernest@validmeasures.org>
2019-01-25 07:13:15 +0200

    update countryCentroids.R

--- data-raw/countryCentroids.R
+++ data-raw/countryCentroids.R
@@ -10,6 +10,8 @@ register_google(key = "AIzaSyCa7K4CUhyOLBJUrLZPm4qEwv5yyP9zMKQ")
 
 countryCentroid <- data.frame("country" = list_countries$country, geocode(list_countries$country))
 
+write.csv(countryCentroid, "data-raw/countryCentroid.csv", row.names = FALSE)
+
 usethis::use_data(countryCentroid, overwrite = TRUE)
 
 
