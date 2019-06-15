ec82d3e5ee1e4389c01109e285c68b12ebeeee46
Ernest Guevarra <ernest@validmeasures.org>
2019-06-15 07:23:32 +0100

    update test for calculate_area

--- tests/testthat/test-calculate.R
+++ tests/testthat/test-calculate.R
@@ -3,7 +3,8 @@ test_that("calculate_area works", {
   expect_is(calculate_area(d = 10), "data.frame")
 })
 
-test_that("tri.area correct", {
+test_that("calculate_area correct", {
   expect_equal(calculate_area(d = 10)[ , 1], tan(30 * (pi / 180)) * (9 / 4) * 10 ^ 2)
   expect_equal(calculate_area(d = 10)[ , 2], ((3 * sqrt(3)) / 2) * 10 ^ 2)
+  expect_equal(calculate_area(d = 10, digits = 2)[ , 1], 129.90)
 })
