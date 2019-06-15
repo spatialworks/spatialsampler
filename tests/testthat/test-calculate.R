37a2b3125f391d83b68c2a7017094ab698d26047
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-06-15 08:04:46 +0100

    update test for calculate_d

--- tests/testthat/test-calculate.R
+++ tests/testthat/test-calculate.R
@@ -1,10 +1,16 @@
 test_that("calculate_area works", {
   expect_equal(length(calculate_area(d = 10)), 2)
   expect_is(calculate_area(d = 10), "data.frame")
-})
-
-test_that("calculate_area correct", {
   expect_equal(calculate_area(d = 10)[ , 1], tan(30 * (pi / 180)) * (9 / 4) * 10 ^ 2)
   expect_equal(calculate_area(d = 10)[ , 2], ((3 * sqrt(3)) / 2) * 10 ^ 2)
   expect_equal(calculate_area(d = 10, digits = 2)[ , 1], 129.90)
 })
+
+test_that("calculate_d works", {
+  expect_error(calculate_d(area = 130), "Polygon type needs to be specified. Try again.")
+  expect_equal(calculate_d(area = 130, geom = "tri"), sqrt((130 * 4) / (tan(30 * (pi / 180)) * 9)))
+  expect_equal(calculate_d(area = 130, geom = "hex"), sqrt((130 * 2) / (3 * sqrt(3))))
+})
+
+
+
