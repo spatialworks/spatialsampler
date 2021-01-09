5af44d962ed5048b191ee020e121834581d98f36
Ernest Guevarra <ernest@guevarra.io>
2021-01-09 10:26:02 +0000

    change calculation for find_utm

--- R/find_utm_zone.R
+++ R/find_utm_zone.R
@@ -16,15 +16,32 @@
 ################################################################################
 
 find_utm_zone <- function(lon, lat) {
-  if (lat >= 72.0 & lat < 84.0 )
-    if (lon >= 0.0 & lon <  9.0)
-      return(31);
-  if (lon >= 9.0 & lon < 21.0)
-    return(33)
-  if (lon >= 21.0 & lon < 33.0)
-    return(35)
-  if (lon >= 33.0 & lon < 42.0)
-    return(37)
+  if (lat > 55 & lat < 64 & lon > 2 & lon < 6) zone <- 32
 
-  (floor((lon + 180) / 6) %% 60) + 1
+  if (lat > 71 & lon >= 6 & lon < 9) zone <- 31
+
+  if (lat > 71 & lon >= 9 & lon < 12) zone <- 33
+
+  if (lat > 71 & lon >= 18 & lon < 21) zone <- 33
+
+  if (lat > 71 & lon >= 21 & lon < 24) zone <- 35
+
+  if (lat > 71 & lon >= 30 & lon < 33) zone <- 35
+
+  zone <- (floor((lon + 180) / 6) %% 60) + 1
+
+  return(zone)
+
+
+  #if (lat >= 72.0 & lat < 84.0 )
+  #  if (lon >= 0.0 & lon <  9.0)
+  #    return(31);
+  #if (lon >= 9.0 & lon < 21.0)
+  #  return(33)
+  #if (lon >= 21.0 & lon < 33.0)
+  #  return(35)
+  #if (lon >= 33.0 & lon < 42.0)
+  #  return(37)
+
+  #(floor((lon + 180) / 6) %% 60) + 1
 }
