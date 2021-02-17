4e47654006b427416a883e40f762f95569fb8e2c
Ernest Guevarra <ernest@guevarra.io>
2021-02-17 07:09:27 +0000

    update map projections dataset

--- data-raw/proj4string.R
+++ data-raw/proj4string.R
@@ -1,6 +1,7 @@
 country <- c("Sudan", "Ethiopia", "Kenya", "Somalia", "Uganda", "Rwanda",
              "Zambia", "Nigeria", "Niger", "Burkina Faso", "Liberia",
-             "Sierra Leone", "Mozambique", "Tanzania")
+             "Sierra Leone", "Mozambique", "Tanzania", "Jordan", "Philippines",
+             "South Africa")
 
 utm <- c("Adindan / UTM zone 35N",
          "Adindan / UTM zone 38N",
@@ -15,7 +16,10 @@ utm <- c("Adindan / UTM zone 35N",
          "Liberia 1964",
          "Sierra Leone 1968 / UTM zone 29N",
          "Moznet / UTM zone 38S",
-         "Arc 1960 / UTM zone 37S")
+         "Arc 1960 / UTM zone 37S",
+         "ED50 / UTM zone 37N",
+         "WGS 84 / UTM zone 51N",
+         "WGS 84 / UTM zone 34S")
 
 proj <- c("+proj=utm +zone=35 +ellps=clrk80 +towgs84=-166,-15,204,0,0,0,0 +units=m +no_defs",
           "+proj=utm +zone=38 +ellps=clrk80 +towgs84=-166,-15,204,0,0,0,0 +units=m +no_defs ",
@@ -30,9 +34,13 @@ proj <- c("+proj=utm +zone=35 +ellps=clrk80 +towgs84=-166,-15,204,0,0,0,0 +units
           "+proj=utm +zone=29 +ellps=WGS84 +datum=WGS84 +units=m +no_defs",
           "+proj=utm +zone=28 +ellps=clrk80 +towgs84=-88,4,101,0,0,0,0 +units=m +no_defs",
           "+proj=utm +zone=38 +south +ellps=WGS84 +towgs84=0,0,0,-0,-0,-0,0 +units=m +no_defs",
-          "+proj=utm +zone=37 +south +ellps=clrk80 +towgs84=-160,-6,-302,0,0,0,0 +units=m +no_defs")
+          "+proj=utm +zone=37 +south +ellps=clrk80 +towgs84=-160,-6,-302,0,0,0,0 +units=m +no_defs",
+          "+proj=utm +zone=37 +ellps=intl +towgs84=-87,-98,-121,0,0,0,0 +units=m +no_defs",
+          "+proj=utm +zone=51 +datum=WGS84 +units=m +no_defs",
+          "+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs")
 
-epsg <- c("20135", "20138", "21097", "20539", "21095", "32736", "20934", "26322", "32631", "32630", "4251", "2162", "5629", "21037")
+epsg <- c("20135", "20138", "21097", "20539", "21095", "32736", "20934", "26322",
+          "32631", "32630", "4251", "2162", "5629", "21037", "23037", "32651", "32734")
 
 map_projections <- data.frame(country, utm, proj, epsg)
 
